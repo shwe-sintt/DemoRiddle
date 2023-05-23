70c2b80ca736f90c61063c86b10b13117a503f4c
22003619 <22003619@myrp.edu.sg>
2023-05-23 15:44:20 +0800

    3:43 edit

--- app/src/main/java/sg/edu/rp/c346/id22003619/demoriddle/AnswerActivity1.java
+++ app/src/main/java/sg/edu/rp/c346/id22003619/demoriddle/AnswerActivity1.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.TelephonyCallback;
+import android.util.Log;
 import android.widget.TextView;
 
 public class AnswerActivity1 extends AppCompatActivity {
     TextView tvAnswer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("AnswerActivity1", "onCreate() called.");
         super.onCreate(savedInstanceState);
+        Log.d("AnswerActivity1", "onCreate() called.");
         setContentView(R.layout.activity_answer1);
         tvAnswer=findViewById(R.id.textView);
         //tvAnswer.setText("In Second Activity");
@@ -21,4 +24,35 @@ public class AnswerActivity1 extends AppCompatActivity {
         tvAnswer.setText(questionsSelected + " answer is: Queue");
 
     }
+    @Override
+    protected void onStart() {
+        Log.d("AnswerActivity1", "onStart() called.");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        Log.d("AnswerActivity1", "onResume() called.");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.d("AnswerActivity1", "onPause() called.");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.d("AnswerActivity1", "onStop() called.");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.d("AnswerActivity1", "onDestroy() called.");
+        super.onDestroy();
+    }
+    @Override
+    protected void onRestart() {
+        Log.d("AnswerActivity1", "onRestart() called.");
+        super.onRestart();
+    }
+
 }
--- app/src/main/java/sg/edu/rp/c346/id22003619/demoriddle/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22003619/demoriddle/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnRevealQ2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("MainActivity", "onCreate() called.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvQ1=findViewById(R.id.textViewQ1);
@@ -42,4 +44,35 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    @Override
+    protected void onStart() {
+        Log.d("MainActivity", "onStart() called.");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        Log.d("MainActivity", "onResume() called.");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.d("MainActivity", "onPause() called.");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.d("MainActivity", "onStop() called.");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.d("MainActivity", "onDestroy() called.");
+        super.onDestroy();
+    }
+    @Override
+    protected void onRestart() {
+        Log.d("MainActivity", "onRestart() called.");
+        super.onRestart();
+    }
+
 }
